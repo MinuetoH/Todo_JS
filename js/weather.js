6f3ab7ee8ab15f3ef70995d7cdbae39e458f52be
kim <kim@kimui-MacBookAir.local>
2023-03-28 21:11:47 +0900

    commit

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "1ae206d18f9cb8d4a4f488d6a8246f5e";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
